5322df06e3ea1a3c574079417615d423bb98d94a
wobsoriano <sorianorobertc@gmail.com>
2023-09-19 12:24:55 -0700

    style: switch to biome

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,51 @@
-import path from 'node:path'
-import fs from 'node:fs'
-import { getTsconfig } from 'get-tsconfig'
-import { generateDtsBundle, type CompilationOptions, type EntryPointConfig } from 'dts-bundle-generator'
+import fs from 'node:fs';
+import path from 'node:path';
+import {
+	type CompilationOptions,
+	type EntryPointConfig,
+	generateDtsBundle,
+} from 'dts-bundle-generator';
+import { getTsconfig } from 'get-tsconfig';
 
 type Options = Omit<EntryPointConfig, 'filePath'> & {
-  compilationOptions?: CompilationOptions
-}
+	compilationOptions?: CompilationOptions;
+};
 
 const dts = (options?: Options): import('bun').BunPlugin => {
-  return {
-    name: 'bun-plugin-dts',
-    async setup(build) {
-      const { compilationOptions, ...rest } = options || {}
+	return {
+		name: 'bun-plugin-dts',
+		async setup(build) {
+			const { compilationOptions, ...rest } = options || {};
 
-      const entrypoints = [...build.config.entrypoints].sort()
-      const entries = entrypoints.map((entry) => {
-        return {
-          filePath: entry,
-          ...rest
-        }
-      })
+			const entrypoints = [...build.config.entrypoints].sort();
+			const entries = entrypoints.map((entry) => {
+				return {
+					filePath: entry,
+					...rest,
+				};
+			});
 
-      const tsconfig = compilationOptions?.preferredConfigPath ?? getTsconfig()?.path
-      const result = generateDtsBundle(entries, {
-        ...compilationOptions,
-        preferredConfigPath: tsconfig
-      })
+			const tsconfig =
+				compilationOptions?.preferredConfigPath ?? getTsconfig()?.path;
+			const result = generateDtsBundle(entries, {
+				...compilationOptions,
+				preferredConfigPath: tsconfig,
+			});
 
-      const outDir = build.config.outdir || './dist'
-      if (!fs.existsSync(outDir)) {
-        fs.mkdirSync(outDir)
-      }
+			const outDir = build.config.outdir || './dist';
+			if (!fs.existsSync(outDir)) {
+				fs.mkdirSync(outDir);
+			}
 
-      await Promise.all(
-        entrypoints.map((entry, index) => {
-          const dtsFile = entry.replace(/^.*\//, '').replace(/\.ts$/, '.d.ts')
-          const outFile = path.join(outDir, dtsFile)
-          return Bun.write(outFile, result[index])
-        })
-      )
-    }
-  }
-}
+			await Promise.all(
+				entrypoints.map((entry, index) => {
+					const dtsFile = entry.replace(/^.*\//, '').replace(/\.ts$/, '.d.ts');
+					const outFile = path.join(outDir, dtsFile);
+					return Bun.write(outFile, result[index]);
+				}),
+			);
+		},
+	};
+};
 
-export default dts
+export default dts;
