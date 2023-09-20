9dc0a34b4c31571c888b02fe60318962b3927c9c
wobsoriano <sorianorobertc@gmail.com>
2023-09-20 08:00:17 -0700

    style: biome format

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,9 @@ const dts = (options?: Options): import('bun').BunPlugin => {
 
 			await Promise.all(
 				entrypoints.map((entry, index) => {
-					const dtsFile = entry.replace(/^.*\//, '').replace(/\.[jt]s$/, '.d.ts');
+					const dtsFile = entry
+						.replace(/^.*\//, '')
+						.replace(/\.[jt]s$/, '.d.ts');
 					const outFile = path.join(outDir, dtsFile);
 					return Bun.write(outFile, result[index]);
 				}),
